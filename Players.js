0ecffa9f0a1597016554bb96102400c75cf41316
Voffi <Voffi910@outlook.com>
2026-08-12 09:56:58 +0200

    Create Players.js with player data
    Added initial player data with attributes and categories.

---
+++ Players.js
@@ -0,0 +1,21 @@
+export const players = [
+  { 
+    id:0, 
+    name:"Test", 
+    avatar:"https://yeeplist.page.gd/images/placeholder.png", 
+    title:"", 
+    score:0+0+0+0+0+0+0+0, 
+    rank:"https://yeeplist.page.gd/images/Newb.png",
+    categories:[
+      {cat:"Double Bat", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:"Shield", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:"Freeze Glove", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:"Dagger", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:"Scythe", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:"Fire Axe", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:"Spear", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:"Mace", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:"Custom", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"}
+    ]
+  },
+  ]
