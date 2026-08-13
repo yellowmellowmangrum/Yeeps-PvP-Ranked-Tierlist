37af6690a535672f6c3b87bfe1e3b62a39be2e7c
Voffi <Voffi910@outlook.com>
2026-08-13 08:58:10 +0200

    Modify player attributes in Players.js
    Updated player details including name, title, score, and categories.

--- Players.js
+++ Players.js
@@ -1,21 +1,21 @@
 export const players = [
   { 
     id:0, 
-    name:"Test", 
+    name:"Yellowmellow13", 
     avatar:"https://yeeplist.page.gd/images/placeholder.png", 
-    title:"", 
-    score:0+0+0+0+0+0+0+0, 
-    rank:"images/CN.webp",
+    title:"Combat Grandmaster", 
+    score:90+100+100+80+80+70+60+80+70, 
+    rank:"images/GM.webp",
     categories:[
-      {cat:"Double Bat", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"Shield", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"Freeze Glove", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"Dagger", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"Scythe", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"Fire Axe", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"Spear", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"Mace", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
-      {cat:"Custom", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"}
+      {cat:"Double Bat", pos:"Low Tier 1", img:"images/Bat.webp"},
+      {cat:"Shield", pos:"High Tier 1", img:"images/Shield.webp"},
+      {cat:"Freeze Glove", pos:"High Tier 1", img:"images/Freeze.webp"},
+      {cat:"Dagger", pos:"High Tier 2", img:"images/Dagger.webp"},
+      {cat:"Scythe", pos:"High Tier 2", img:"images/Scythe.webp"},
+      {cat:"Fire Axe", pos:"Low Tier 2", img:"images/Axe.webp"},
+      {cat:"Spear", pos:"High Tier 3", img:"images/Spear.webp"},
+      {cat:"Mace", pos:"High Tier 2", img:"images/Mace.webp"},
+      {cat:"Custom", pos:"Low Tier 2", img:"images/All.webp"}
     ]
   },
   ]
