567fa40557988598ed0eca030954467b183de25b
Voffi <Voffi910@outlook.com>
2026-08-13 09:03:55 +0200

    Add new player 'Littleeasy1a' with details

--- Players.js
+++ Players.js
@@ -18,4 +18,23 @@ export const players = [
       {cat:"Custom", pos:"Low Tier 2", img:"images/All.webp"}
     ]
   },
+  { 
+    id:1, 
+    name:"Littleeasy1a", 
+    avatar:"https://yeeplist.page.gd/images/placeholder.png", 
+    title:"Combat Master", 
+    score:70+60+100+50+70+80+40+80+90, 
+    rank:"images/CM.webp",
+    categories:[
+      {cat:"Double Bat", pos:"Low Tier 2", img:"images/Bat.webp"},
+      {cat:"Shield", pos:"High Tier 3", img:"images/Shield.webp"},
+      {cat:"Freeze Glove", pos:"High Tier 1", img:"images/Freeze.webp"},
+      {cat:"Dagger", pos:"Low Tier 3", img:"images/Dagger.webp"},
+      {cat:"Scythe", pos:"Low Tier 2", img:"images/Scythe.webp"},
+      {cat:"Fire Axe", pos:"High Tier 2", img:"images/Axe.webp"},
+      {cat:"Spear", pos:"High Tier 4", img:"images/Spear.webp"},
+      {cat:"Mace", pos:"High Tier 2", img:"images/Mace.webp"},
+      {cat:"Custom", pos:"Low Tier 1", img:"images/All.webp"}
+    ]
+  },
   ]
